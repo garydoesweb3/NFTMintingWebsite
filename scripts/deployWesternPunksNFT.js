d931414146f117aab4dd4d3942e478e70aa93b2d
garydoesweb3 <garydoesweb3@gmail.com>
2022-07-07 12:33:28 -0500

    initial commit

---
+++ scripts/deployWesternPunksNFT.js
@@ -0,0 +1,20 @@
+
+const hre = require("hardhat");
+
+async function main() {
+  const WesternPunksNFT = await hre.ethers.getContractFactory("WesternPunksNFT");
+  const westernPunksNFT = await WesternPunksNFT.deploy();
+
+  await westernPunksNFT.deployed();
+
+  console.log("WesternPunksNFT deployed to:", westernPunksNFT.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
